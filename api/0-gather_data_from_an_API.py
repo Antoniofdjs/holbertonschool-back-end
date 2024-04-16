f49add50030ed5241ca37f294ed4c7249972fb3b
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-16 09:01:28 -0400

    Another test using .json after get

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,50 +1,47 @@
 #!/usr/bin/python3
-'''
+"""
     Api REST
-'''
+"""
 
-if __name__ == '__main__':
-    import requests
-    from sys import argv
+import requests
+from sys import argv
 
-    '''
-        Using this REST API, for a given employee ID,
-        returns information about his/her TODO list progress.
 
-        Args:
-            id (int): The employee ID.
-
-        Returns:
-            None
-    '''
+def get_employee(id=None):
+    """
+    using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress.
+    """
+    # check if argv[1] is a number int
     if len(argv) > 1:
         try:
             id = int(argv[1])
         except ValueError:
-            pass
+            return
 
     if isinstance(id, int):
-        user = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}")
-        to_dos = requests.get(
-            f"https://jsonplaceholder.typicode.com/todos/?userId={id}"
-            )
+        base = "https://jsonplaceholder.typicode.com"
+        user = requests.get(f"{base}/users/{id}").json()
+        to_dos = requests.get(f"{base}/todos/?userId={id}").json()
 
-        if to_dos.status_code == 200 and user.status_code == 200:
-            user = user.json()
-            to_dos = to_dos.json()
-
+        if user and to_dos:
             total_tasks = len(to_dos)
-            tasks_completed = 0
-            titles_completed = []
-
-            for to_do in to_dos:
-                if to_do.get('completed'):
-                    tasks_completed += 1
-                    titles_completed.append(to_do.get('title'))
-
+            # fmt: off
+            titles_completed = [task["title"]
+                                for task in to_dos
+                                if task["completed"]]
+            # fmt: on
             tasks_completed = len(titles_completed)
 
-            print(f"Employee {user.get('name')} is done with tasks\
-                  ({tasks_completed}/{total_tasks})")
+            print(
+                "Employee {} is done with tasks({}/{}):".format(
+                    user["name"], tasks_completed, total_tasks
+                )
+            )
+
             for title in titles_completed:
                 print(f"\t {title}")
+
+
+if __name__ == "__main__":
+    get_employee()
